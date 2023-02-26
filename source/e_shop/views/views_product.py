ddc3b1f23141c34b2870f83e70d5c3793d167804
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-26 18:13:31 +0600

    добавленно передставление добаления и фильтрация на главную

--- source/e_shop/views/views_product.py
+++ source/e_shop/views/views_product.py
@@ -1,9 +1,22 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
+from e_shop.forms import ProductForm
 from e_shop.models import Product
 
 
 def index_view(request: WSGIRequest):
-    products = Product.objects.all()
+    products = Product.objects.filter(remainder__gt=0)
     return render(request, 'index.html', context={'products': products})
+
+
+def create_product(request: WSGIRequest):
+    if request.method == 'GET':
+        form = ProductForm()
+        return render(request, 'add_product.html', context={'form': form})
+    form = ProductForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    return render(request, 'add_product.html', context={'form': form})
+
