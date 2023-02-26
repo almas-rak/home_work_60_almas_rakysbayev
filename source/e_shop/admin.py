11c5362e6f3ee7e771e17c7b9bc15815423472a8
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-26 11:03:23 +0600

    регистрация модели продукта в админке

--- source/e_shop/admin.py
+++ source/e_shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from e_shop.models import Product
+
+
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'description', 'image', 'category', 'remainder', 'price')
+    list_editable = ('product', 'description', 'category', 'remainder', 'price')
+
+
+admin.site.register(Product, ProductAdmin)
