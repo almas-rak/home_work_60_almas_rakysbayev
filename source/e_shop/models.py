6c3d0c757e67bb8bbd7576df9bb0f3d9520c6215
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-26 10:59:44 +0600

    решил не уходить от ТЗ

--- source/e_shop/models.py
+++ source/e_shop/models.py
@@ -3,36 +3,22 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
-    category = models.CharField(max_length=100, verbose_name='Категория')
-    description = models.CharField(max_length=1000, null=True, blank=True, verbose_name='Описание')
-
-    class Meta:
-        ordering = ['id']
-
-    def __str__(self):
-        return self.category
-
-
-class ChildCategory(models.Model):
-    category = models.CharField(max_length=100, verbose_name='Категория')
-    description = models.CharField(max_length=1000, null=True, blank=True, verbose_name='Описание')
-    parent = models.ForeignKey(Category, on_delete=models.RESTRICT, verbose_name='Родительская категория')
-
-    class Meta:
-        ordering = ['id']
-
-    def __str__(self):
-        return self.category
-
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('food', 'Еда'),
+        ('clothes', 'Одежда'),
+        ('other', 'Разное')
+    ]
     product = models.CharField(max_length=100, verbose_name='Продукт')
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
     image = models.CharField(max_length=3000, verbose_name='Ссылка на фото')
-    category = models.ForeignKey(ChildCategory, on_delete=models.RESTRICT, verbose_name='Категория')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='other', verbose_name='Категория')
     remainder = models.PositiveIntegerField(verbose_name='Остаток')
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+
+    class Meta:
+        ordering = ['category', 'product']
 
     def __str__(self):
         return self.product
