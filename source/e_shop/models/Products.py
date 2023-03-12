0d2407b156631f332a74e987552d4ace34ff03ad
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-12 21:10:19 +0600

    добавлено мягкое удаление, убрана сортировка

--- source/e_shop/models/Products.py
+++ source/e_shop/models/Products.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -16,10 +17,14 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='other', verbose_name='Категория')
     remainder = models.PositiveIntegerField(verbose_name='Остаток')
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+    is_deleted = models.BooleanField(default=False, null=True, verbose_name='Удалено')
+    deleted_at = models.DateTimeField(null=True, verbose_name='Дата и время удаления')
 
-    class Meta:
-        ordering = ['category', 'product']
-        
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
     def get_category_display(self):
         return dict(self.CATEGORY_CHOICES)[self.category]
 
